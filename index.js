5898462a9bcd3f8452ab6a21cf8f9445307667f3
Denisha Garner <thegarnercode@gmail.com>
2022-03-28 00:51:03 -0400

    commit tree 9ffea516133e8cfcc90a45144043be30468be843

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b){
+return a + b;
+}
+function subtract(a, b){
+    return a - b;
+    
+}
+function multiply(a, b){
+    return  a * b;
+    
+}
+function divide(a, b){
+    return a / b;
+    
+}
+
+
+
+function number(){
+ for(let i=0 i>5 i++)
+        return (number)
+    }
+}
+
